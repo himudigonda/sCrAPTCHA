e71295e6f8032a7a7a07abba55ed5acc800a3117
ruhend <mudigonda.himanshh@gmail.com>
2021-06-07 21:23:26 +0530

    changed working of initial caption generator

--- src/stringGenerator/init_cap_gen.py
+++ src/stringGenerator/init_cap_gen.py
@@ -9,6 +9,7 @@ import re
 
 debugFlag = 1
 
+
 def __dout(status, output):
     if debugFlag:
         print(' ' + status + ' ' + output)
@@ -18,20 +19,25 @@ def __dout(status, output):
 
 ### Write Code From Here ###
 class init_cap_gen:
-    def genstr(length):
-        defRange = 'abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*\(\)'
-        randomStr = ''.join(random.choice(defRange) for letters in range(length))
-        return randomStr
 
     def genlength():
-        maxLength = [4,5,6,7]
+        maxLength = [4, 5, 6, 7]
         pickNumber = random.choice(maxLength)
         return pickNumber
-        
-    length_of_captcha = genlength()
-    print(genstr(length_of_captcha))
+    # init_cap_gen.genstr(pickNumber)
+
+    def genstr():
+        lengthOfCaption = init_cap_gen.genlength()
+        defRange = 'abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*\(\)'
+        randomStr = ''.join(random.choice(defRange)
+                            for letters in range(lengthOfCaption))
+        return randomStr
+
+    # genlength()
+    # length_of_captcha = genlength()
+    # print(init_cap_gen.genstr(length_of_captcha))
     if __name__ == '__main__':
         pass
-        
+
 # print(genlength())
 ### Code End Here ###
