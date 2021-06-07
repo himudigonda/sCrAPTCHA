2aa0b5487af907926c61d464c9b0337f6e471a8a
ruhend <mudigonda.himanshh@gmail.com>
2021-06-07 13:17:39 +0530

    moved handwriting into dir

--- src/handwriting/txt2handwriting.py
+++ src/handwriting/txt2handwriting.py
@@ -1,3 +1,4 @@
+#! /usr/local/opt/python@3.9/bin/python3.9
 # 2021-06-06 14:37:50
 #! @author : @ruhend
 
@@ -15,7 +16,7 @@ def __dout(status, output):
 
 ### Write Code From Here ###
 
-pywhatkit.text_to_handwriting("Hello world! My name is Himansh Mudigonda")
+pywhatkit.text_to_handwriting("Hello world! My name is Himansh Mudigonda","src/handwriting/alpha.py",[255,0,0])
 
 
 ### Code End Here ###
