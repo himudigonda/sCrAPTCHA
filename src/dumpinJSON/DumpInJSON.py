3e4e2fb77651a031dd83ff37421b6d0e07345d27
ruhend <mudigonda.himanshh@gmail.com>
2021-06-09 12:06:05 +0530

    introducing JSONdump for dict

---
+++ src/dumpinJSON/DumpInJSON.py
@@ -0,0 +1,17 @@
+#! /usr/local/opt/python@3.9/bin/python3.9
+# 2021-06-07 13:26:41
+#! @author : @ruhend
+
+# imports here
+
+
+# global variables here
+
+
+### Write Code From Here ###
+class DumpInJSON():
+
+    if __name__ == '__main__':
+        pass
+
+### Code End Here ###
