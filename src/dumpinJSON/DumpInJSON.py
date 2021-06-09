85bf3064166a44e8d7b1b79246cf576ea155662d
ruhend <mudigonda.himanshh@gmail.com>
2021-06-09 12:18:13 +0530

    concluded jsondumper is better suited

--- src/dumpinJSON/DumpInJSON.py
+++ src/dumpinJSON/DumpInJSON.py
@@ -12,9 +12,9 @@ import pickle
 ### Write Code From Here ###
 class DumpInJSON():
 
-    def PickleDumper(_alphanumeric_hash_dict):
-        with open('AN-HASH_pickle.json', 'wb') as fp:
-            pickle.dump(_alphanumeric_hash_dict, fp)
+    # def PickleDumper(_alphanumeric_hash_dict):
+    #     with open('AN-HASH_pickle.json', 'wb') as fp:
+    #         pickle.dump(_alphanumeric_hash_dict, fp)
     
     def JSONDumper(_alphanumeric_hash_dict):
         with open('AN-HASH_json.json','w') as rp:
@@ -23,7 +23,7 @@ class DumpInJSON():
     temp_dict = {'sdfasdf':'sduyq234h5iufhsgu2y3u4ihj5hrgyfgasdfguq234',
                  'dfjgh':'wu4y97berwutyqnv9384t3ydsktupq24oiu2gsdg'
                  }
-    PickleDumper(temp_dict)
+    # PickleDumper(temp_dict)
     JSONDumper(temp_dict)
 
     if __name__ == '__main__':
