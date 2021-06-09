719cb89e232be4b649c86cf4ff7685c3631551ed
ruhend <mudigonda.himanshh@gmail.com>
2021-06-09 11:54:08 +0530

    Added Make Hash to get hash

---
+++ src/myhasher/MakeHash.py
@@ -0,0 +1,31 @@
+#! /usr/local/opt/python@3.9/bin/python3.9
+# 2021-06-08 11:56:09
+#! @author : @ruhend
+
+# imports here
+import hashlib
+
+# global variables here
+
+### Write Code From Here ###
+
+
+class MakeHash:
+    def _EncodeString(_string):
+        encoded_string = _string.encode()
+        return encoded_string
+
+    def _SHA256This(_string):
+        sha256ed = hashlib.sha256(_string)
+        return sha256ed
+
+    def _HashTheCode(_string):
+        hashed_value = MakeHash._SHA256This(MakeHash._EncodeString(_string))
+        hashed_value_in_hex = hashed_value.hexdigest()
+        return hashed_value_in_hex
+
+    if __name__ == '__main__':
+        pass
+
+
+### Code End Here ###
