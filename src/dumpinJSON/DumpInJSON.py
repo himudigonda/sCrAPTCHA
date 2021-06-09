0b59a166b7d2a6a615c948f0f76efe8f32fb4e62
ruhend <mudigonda.himanshh@gmail.com>
2021-06-09 12:17:41 +0530

    coded and testing with jsondump and pickledump with temp_dict

--- src/dumpinJSON/DumpInJSON.py
+++ src/dumpinJSON/DumpInJSON.py
@@ -3,13 +3,28 @@
 #! @author : @ruhend
 
 # imports here
-
+import json
+import pickle
 
 # global variables here
 
 
 ### Write Code From Here ###
 class DumpInJSON():
+
+    def PickleDumper(_alphanumeric_hash_dict):
+        with open('AN-HASH_pickle.json', 'wb') as fp:
+            pickle.dump(_alphanumeric_hash_dict, fp)
+    
+    def JSONDumper(_alphanumeric_hash_dict):
+        with open('AN-HASH_json.json','w') as rp:
+            json.dump(_alphanumeric_hash_dict, rp)
+    
+    temp_dict = {'sdfasdf':'sduyq234h5iufhsgu2y3u4ihj5hrgyfgasdfguq234',
+                 'dfjgh':'wu4y97berwutyqnv9384t3ydsktupq24oiu2gsdg'
+                 }
+    PickleDumper(temp_dict)
+    JSONDumper(temp_dict)
 
     if __name__ == '__main__':
         pass
