bb44b4735577a86471efc502c3c2b1dcc4aadcbe
ruhend <mudigonda.himanshh@gmail.com>
2021-06-10 16:41:20 +0530

    integrated MakeMyDict

--- src/CallerLookOut.py
+++ src/CallerLookOut.py
@@ -6,6 +6,7 @@
 from mycaptcha import MakeCaptcha
 from mycode import MakeCode
 from myhasher import MakeHash
+from mydict import MakeMyDict
 
 # global variables here
 
@@ -24,6 +25,7 @@ class CallerLookOut:
         code_maker_handler = MakeCode.MakeMyCode
         captcha_maker_handler = MakeCaptcha.MakeMyCaptcha
         hash_maker_handler = MakeHash.MakeHash
+        dict_maker_handler = MakeMyDict.MakeMyDict
 
         # Make alphanumeric code
         _received_code = code_maker_handler._generate_alphanumeric()
@@ -32,18 +34,31 @@ class CallerLookOut:
         hash_of_code = hash_maker_handler._HashTheCode(_received_code)
         print(_received_code + ':' + hash_of_code)
 
+        # Make a dictionary of the values
+        dict_maker_handler.Add2Dict(hash_of_code,_received_code)
+
         #! Change this later on to recurring file name numbers.
         _save_captcha_location = __automated_path__+__file_name__
+
+
 
         # Save Captcha @_save_captcha_location
         captcha_maker_handler._draftCaptcha(
             _received_code, _save_captcha_location)
 
+        
+
     if __name__ == '__main__':
+        # dict_maker_handler = MakeMyDict.MakeMyDict
+        
         # Input your number of files here
         _net_captchas = 5
         for _each_captcha in range(0, _net_captchas):
             MainContainer(_each_captcha)
+        # dict_maker_handler.Print4romDict()
+        
+        
+
 
 
 ### Code End Here ###
