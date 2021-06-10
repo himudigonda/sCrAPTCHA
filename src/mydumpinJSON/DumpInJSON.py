1dd6a17d998f986082c4f4fca906717c072eadec
ruhend <mudigonda.himanshh@gmail.com>
2021-06-10 17:11:33 +0530

    changed dumper->appender

--- src/mydumpinJSON/DumpInJSON.py
+++ src/mydumpinJSON/DumpInJSON.py
@@ -7,20 +7,29 @@ import json
 import pickle
 
 # global variables here
-__save_json_at_ = '../../storage/AN-HASH_json.json'
+__AN_hash_dict__ = {'AlphaNumeric': 'SHA256'}
 
 ### Write Code From Here ###
 
 
 class DumpInJSON():
 
-    # def PickleDumper(_alphanumeric_hash_dict):
-    #     with open('AN-HASH_pickle.json', 'wb') as fp:
-    #         pickle.dump(_alphanumeric_hash_dict, fp)
+    __save_json_at_ = '../../Storage/AN-HASH_json.json'
+
+   # def PickleDumper(_alphanumeric_hash_dict):
+   #     with open('AN-HASH_pickle.json', 'wb') as fp:
+   #         pickle.dump(_alphanumeric_hash_dict, fp)
 
     def JSONDumper(_alphanumeric_hash_dict):
-        with open('../../storage/AN-HASH_json.json', 'w') as rp:
+        with open("../Storage/AN-HASH_json.json", 'w') as rp:
             json.dump(_alphanumeric_hash_dict, rp)
+
+    def JSONAppender(_alphanumeric_hash_dict):
+        with open("../Storage/AN-HASH_json.json", 'r+') as JSONFile:
+            data = json.load(JSONFile)
+            data.update(_alphanumeric_hash_dict)
+            JSONFile.seek(0)
+            json.dump(data, JSONFile)
 
     # temp_dict = {'sdfasdf':'sduyq234h5iufhsgu2y3u4ihj5hrgyfgasdfguq234',
     #              'dfjgh':'wu4y97berwutyqnv9384t3ydsktupq24oiu2gsdg'
@@ -28,6 +37,7 @@ class DumpInJSON():
     # JSONDumper(temp_dict)
 
     if __name__ == '__main__':
+        # JSONDumper(__AN_hash_dict__)
         pass
 
 ### Code End Here ###
